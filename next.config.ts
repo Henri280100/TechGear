b969732499f313da316ae5076f13706ba6eba146
Pkhoa@280100 <69888512+Henri280100@users.noreply.github.com>
2025-05-20 12:01:08 +0700

    Configures absolute imports
    Configures absolute imports using the `@` alias in webpack
    and sets the `baseUrl` in `tsconfig.json`.
    
    Removes the Netlify Next.js plugin as it is not needed
    for static exports.

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   output: "export",
@@ -32,6 +33,10 @@ const nextConfig: NextConfig = {
       },
 
     ]
+  },
+  webpack(config) {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
   }
 };
 
